142430608880c72f19b64048ec8a3d15467a3b80
GabrielSousaJS <gabriels.santosx@gmail.coom>
2022-11-02 18:22:29 -0300

    Data for audit

--- backend/src/main/java/com/gabrielsousa/dscatalog/entities/Category.java
+++ backend/src/main/java/com/gabrielsousa/dscatalog/entities/Category.java
@@ -3,6 +3,7 @@ package com.gabrielsousa.dscatalog.entities;
 import javax.persistence.*;
 import java.io.Serial;
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Objects;
 
 @Entity
@@ -15,7 +16,10 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updateAt;
     public Category() {
     }
 
@@ -38,6 +42,24 @@ public class Category implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdateAt() {
+        return updateAt;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updateAt = Instant.now();
     }
 
     @Override

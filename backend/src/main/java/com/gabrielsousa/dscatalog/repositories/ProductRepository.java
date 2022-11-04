c36338d02ccb36a0681e69b1a59e5fe5d5f54d8c
GabrielSousaJS <gabriels.santosx@gmail.coom>
2022-11-04 01:25:58 -0300

    Product repository, service, resource, GET Ok

---
+++ backend/src/main/java/com/gabrielsousa/dscatalog/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.gabrielsousa.dscatalog.repositories;
+
+import com.gabrielsousa.dscatalog.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
---
+++ backend/src/main/java/com/gabrielsousa/dscatalog/resources/ProductResource.java
@@ -0,0 +1,56 @@
+package com.gabrielsousa.dscatalog.resources;
+
+import com.gabrielsousa.dscatalog.dto.ProductDTO;
+import com.gabrielsousa.dscatalog.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductResource {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping
+    public ResponseEntity<Page<ProductDTO>> findAllPaged(@RequestParam(name = "page") Integer page,
+                                                         @RequestParam(name = "linesPerPage") Integer linesPerPage,
+                                                         @RequestParam(name = "direction") String direction,
+                                                         @RequestParam(name = "orderBy") String orderBy) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        Page<ProductDTO> list = service.findAllPaged(pageRequest);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
+        ProductDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
+        dto = service.update(id, dto);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}

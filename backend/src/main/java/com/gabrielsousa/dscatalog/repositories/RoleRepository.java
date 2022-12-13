887f087830001029ee4e48b51e38091e599c2388
GabrielSousaJS <gabriels.santosx@gmail.coom>
2022-12-13 11:47:59 -0300

    User CRUD

---
+++ backend/src/main/java/com/gabrielsousa/dscatalog/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.gabrielsousa.dscatalog.repositories;
+
+import com.gabrielsousa.dscatalog.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
---
+++ backend/src/main/java/com/gabrielsousa/dscatalog/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.gabrielsousa.dscatalog.repositories;
+
+import com.gabrielsousa.dscatalog.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
